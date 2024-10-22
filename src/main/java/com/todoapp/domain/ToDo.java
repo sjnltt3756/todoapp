359c42adcd055ddbe528211585a849a967a9943d
sjnltt3756 <sjnltt3756@gmail.com>
2024-10-23 00:13:58 +0900

    Todo domain 작성

---
+++ src/main/java/com/todoapp/domain/ToDo.java
@@ -0,0 +1,24 @@
+package com.todoapp.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity(name = "Todo")
+@Table(name = "todo")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class ToDo {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String todo;
+
+
+
+}
