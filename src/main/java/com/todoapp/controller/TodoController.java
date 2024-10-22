27b4de2602864a4f84977f167c93e753f4601bf6
sjnltt3756 <sjnltt3756@gmail.com>
2024-10-23 00:14:56 +0900

    jpa / 리스트 작성

--- src/main/java/com/todoapp/controller/TodoController.java
+++ src/main/java/com/todoapp/controller/TodoController.java
@@ -1,21 +1,34 @@
 package com.todoapp.controller;
 
-import lombok.extern.log4j.Log4j;
+import com.todoapp.domain.ToDo;
+import com.todoapp.repository.ToDoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class TodoController {
+
+    private final ToDoRepository toDoRepository;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        List<ToDo> todos = toDoRepository.findAll();
+        model.addAttribute("todos",todos);
         return "todos";
     }
 
     @PostMapping("/addTodo")
     public String addTodo(@RequestParam("todo") String todo){
-        System.out.println(todo);
+        ToDo toDo = new ToDo();
+        toDo.setTodo(todo);
+        toDoRepository.save(toDo);
         return "redirect:/";
     }
 
